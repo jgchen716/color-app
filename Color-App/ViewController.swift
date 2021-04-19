f5db60dbec319434ee005388fc1359831bb13084
Jonathan Chen <jgchen@sas.upenn.edu>
2021-04-18 20:25:26 -0400

    implement scrollable color collection

--- Color-App/ViewController.swift
+++ Color-App/ViewController.swift
@@ -6,9 +6,48 @@
 //
 
 import UIKit
+// THIS CODE IS BASED ON THIS TUTORIAL:
+// https://www.journaldev.com/10678/ios-uicollectionview-example-tutorial
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 128, height: 128)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        500
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath) as UICollectionViewCell
+        
+        cell.backgroundColor = self.randomColor()
+    
+        return cell
+    }
+    
+    func randomColor() -> UIColor {
+        // drand generates random double between 0.0 and 1.0
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 
-class ViewController: UIViewController {
-
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
